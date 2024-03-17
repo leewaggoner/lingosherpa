83604199fb50d790860032bf43ae39ea98a78296
Lee Waggoner <lwaggoner@roadrunner.com>
2024-03-17 10:51:07 -0700

    Upgraded to Android Studio Iguana

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,13 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
 }
 
-val translateApiKey: String = gradleLocalProperties(rootDir).getProperty("TRANSLATE_API_KEY")
+val translateApiKey: String = gradleLocalProperties(rootDir, providers).getProperty("TRANSLATE_API_KEY")
 
 android {
     namespace = "com.wreckingballsoftware.lingosherpa"
@@ -61,7 +62,7 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -80,7 +81,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
